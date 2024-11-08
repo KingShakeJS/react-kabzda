825f6a095fb329de5afeb47429d16b17701c2ad6
KingShakeJS <kingshakejs@yandex.ru>
2024-11-08 14:23:59 +0300

    l-22

--- src/components/anyStories/UseEffect.stories.tsx
+++ src/components/anyStories/UseEffect.stories.tsx
@@ -17,12 +17,12 @@ export const SimpleExample = () => {
     useEffect(() => {
         console.log('useEffect срабатывает только при первой отрисовке ')
         document.title = counter.toString()
-    },[])
+    }, [])
 
     useEffect(() => {
         console.log('useEffect срабатывает при первой отрисовке и при изменении зависимостей')
         document.title = counter.toString()
-    },[counter])
+    }, [counter])
 
 
     return (
@@ -36,6 +36,56 @@ export const SimpleExample = () => {
                 setFake(fake + 1)
             }}>fake {fake}
             </button>
+        </div>
+    );
+};
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+    console.log('SetTimeoutExample')
+
+    useEffect(() => {
+        console.log('useEffect')
+        setTimeout(() => {
+            console.log('setTimeout')
+            document.title = counter.toString()
+        }, 1500)
+    }, [counter])
+
+
+
+    return (
+        <div>
+            <h1>Hello, {counter}</h1>
+            <button onClick={() => {
+                setCounter(counter + 1)
+            }}>+
+            </button>
+            <button onClick={() => {
+                setFake(fake + 1)
+            }}>fake {fake}
+            </button>
+        </div>
+    );
+};
+export const SetIntervalExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetIntervalExample')
+
+    useEffect(() => {
+        console.log('useEffect')
+        setInterval(() => {
+            console.log('tick: ', counter )
+            setCounter((state)=>state + 1)
+
+        }, 1000)
+    }, [])
+
+
+    return (
+        <div>
+            <p>{counter}</p>
         </div>
     );
 };
